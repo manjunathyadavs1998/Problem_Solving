cbd1c4aee4f05b03719434fcb4e42e1112d3c5e1
smanjunath <manjunathyadavs1998@gmail.com>
2026-06-27 18:02:38 +0530

    Permutations for Array

--- recursion/src/Permutations.java
+++ recursion/src/Permutations.java
@@ -1,4 +1,6 @@
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Permutations {
@@ -15,15 +17,70 @@ public class Permutations {
             generatePermutations(remainingString, currStr, output);
         }
 
+    }
+
+    public static List<List<Integer>> generateArrayPermutations(int[] arr){
+        List<List<Integer>>output=new ArrayList<>();
+        int n=arr.length;
+        boolean[] flag=new boolean[n];
+        Arrays.fill(flag, false);
+        List<Integer>list=new ArrayList<>();
+        backTrack(arr, output, list, flag);
+        return output;
 
 
+    }
+    public static void backTrack(int[] arr, List<List<Integer>>output, List<Integer>list, boolean[] flag){
+
+        if(list.size()==arr.length){
+            output.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0; i<arr.length; i++){
+            if(flag[i]){
+                continue;
+            }
+            list.add(arr[i]);
+            flag[i]=true;
+            backTrack(arr, output, list, flag);
+            list.removeLast();
+            flag[i]=false;
+        }
+    }
+
+    public static List<List<Integer>> permuteUnique(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>>output=new ArrayList<>();
+        int n=arr.length;
+        boolean[] flag=new boolean[n];
+        Arrays.fill(flag, false);
+        List<Integer>list=new ArrayList<>();
+        backTrackForUnique(arr, output, list, flag);
+        return output;
 
 
+    }
+    public static void backTrackForUnique(int[] arr, List<List<Integer>>output, List<Integer>list, boolean[] flag) {
+
+        if (list.size() == arr.length) {
+            output.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if ((i > 0 && arr[i] == arr[i - 1]) && !flag[i - 1] || (flag[i])) continue;
+            list.add(arr[i]);
+            flag[i] = true;
+            backTrack(arr, output, list, flag);
+            list.removeLast();
+            flag[i] = false;
+        }
     }
 
     public static void main(String[] args) {
         List<String>output=new ArrayList<>();
         generatePermutations("abcd", "",output);
         System.out.println(output);
+
+        System.out.println(generateArrayPermutations(new int[] {1,2,3}));
     }
 }
