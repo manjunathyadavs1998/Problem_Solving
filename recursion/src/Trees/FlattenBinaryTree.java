4dfcefb8832dce4fe8a40798bf75e7dce2d3766e
smanjunath <manjunathyadavs1998@gmail.com>
2026-07-25 18:23:03 +0530

    Tree Constructions

---
+++ recursion/src/Trees/FlattenBinaryTree.java
@@ -0,0 +1,24 @@
+package Trees;
+
+public class FlattenBinaryTree {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+
+            if (curr.left != null) {
+
+                TreeNode pred = curr.left;
+
+                while (pred.right != null) {
+                    pred = pred.right;
+                }
+
+                pred.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+
+            curr = curr.right;
+        }
+    }
+}
