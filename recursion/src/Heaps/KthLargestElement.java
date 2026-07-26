1fdf60f05783a9b2a74b77623088424b9c8a7638
smanjunath <manjunathyadavs1998@gmail.com>
2026-07-26 14:26:29 +0530

    K Frequent Elements

--- recursion/src/Heaps/KthLargestElement.java
+++ recursion/src/Heaps/KthLargestElement.java
@@ -1,5 +1,7 @@
 package Heaps;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class KthLargestElement {
@@ -29,6 +31,34 @@ public class KthLargestElement {
             }
 
         return -1;
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+                new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+
+            pq.offer(entry);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] ans = new int[k];
+
+        for (int i = k - 1; i >= 0; i--) {
+            ans[i] = pq.poll().getKey();
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
